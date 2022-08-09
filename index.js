f40c88a289bffd1200d34d6d82136dd482787e0d
WilliamForest <michel.04.11.00@gmail.com>
2022-08-09 17:58:42 +0300

    feat: done 1 practic

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict';
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 1);
+
+const personalMuvieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const firstQuestion = prompt('Один из просмотренных фильмов?', 'терминатор'),
+      secondQuestion = prompt('На сколько оцените его?', 10),
+      thirdQuestion = prompt('Один из просмотренных фильмов?', 'терминатор'),
+      fourQuestion = prompt('На сколько оцените его?', 10);
+      
+personalMuvieDB.movies[firstQuestion] = secondQuestion; 
+personalMuvieDB.movies[thirdQuestion] = fourQuestion;  
+
+console.log(personalMuvieDB);
