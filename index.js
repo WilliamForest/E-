24000b1f39b8e0983f1ace9e80d587ee6fbdaf9a
WilliamForest <michel.04.11.00@gmail.com>
2022-08-09 20:52:14 +0300

    feat: done part 2

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-'use strict';
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 1);
+// 'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 1);
 
-const personalMuvieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
 };
-const firstQuestion = prompt('Один из просмотренных фильмов?', 'терминатор'),
-      secondQuestion = prompt('На сколько оцените его?', 10),
-      thirdQuestion = prompt('Один из просмотренных фильмов?', 'терминатор'),
-      fourQuestion = prompt('На сколько оцените его?', 10);
-      
-personalMuvieDB.movies[firstQuestion] = secondQuestion; 
-personalMuvieDB.movies[thirdQuestion] = fourQuestion;  
 
-console.log(personalMuvieDB);
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    if (a != '' && b != '' && a != null && b != null && a.length < 50){
+        personalMovieDB.movies[a] = b; 
+        console.log('all done!');
+    } else {
+        console.log('error, try again!');
+            i--;
+        }
+    }
+
+    if (personalMovieDB.count < 10){
+        console.log('Просмотренно давольно мало фильмов');
+    } else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30){
+        console.log('Вы киноман');
+    } else{
+        console.log('Перепроверьте значения');
+    }
+
+console.log(personalMovieDB);
